2175188c1ad50a373f6b1a0e76fbf1d8f891555d
empflow <empflow@tutanota.com>
2023-12-24 11:50:41 +0300

    improve back btn navigation logic

--- src/components/RootLayout/Header/BackBtn.tsx
+++ src/components/RootLayout/Header/BackBtn.tsx
@@ -4,11 +4,18 @@ import { useRouter } from "next/navigation";
 
 export function HeaderBackBtn() {
   const router = useRouter();
+
+  function handleNavigate() {
+    const canNavigateBack = history.length <= 1;
+    if (canNavigateBack) router.push("/");
+    else router.back();
+  }
+
   return (
     <Button
       variant="ghost"
-      onClick={router.back}
       className="aspect-square rounded-full p-1 hover:bg-slate-100 active:bg-slate-200"
+      onClick={handleNavigate}
     >
       <ArrowLeft />
     </Button>
