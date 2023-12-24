944d091c3dd69f6e6e9260e83e250bbbd64111a8
empflow <empflow@tutanota.com>
2023-12-24 12:48:14 +0300

    fix sidebar

--- src/components/RootLayout/Sidebar/Links.tsx
+++ src/components/RootLayout/Sidebar/Links.tsx
@@ -23,27 +23,26 @@ const LINKS = [
 export default function SidebarLinks({ className }: Props) {
   const pathname = usePathname();
   return (
-    <nav
-      className={cn(
-        "flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1",
-        className,
-      )}
-    >
-      {LINKS.map((item) => (
-        <Link
-          key={item.href}
-          href={item.href}
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            pathname === item.href
-              ? "bg-muted hover:bg-muted"
-              : "hover:bg-transparent hover:underline",
-            "justify-start text-lg",
-          )}
-        >
-          {item.title}
-        </Link>
-      ))}
+    <nav className={cn("flex flex-col gap-1", className)}>
+      <ul>
+        {LINKS.map((item) => (
+          <li>
+            <Link
+              key={item.href}
+              href={item.href}
+              className={cn(
+                buttonVariants({ variant: "ghost" }),
+                pathname === item.href
+                  ? "bg-muted hover:bg-muted"
+                  : "hover:bg-transparent hover:underline",
+                "w-full justify-start text-lg",
+              )}
+            >
+              {item.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 }
--- src/components/RootLayout/Sidebar/Sidebar.tsx
+++ src/components/RootLayout/Sidebar/Sidebar.tsx
@@ -4,7 +4,7 @@ import SidebarLinks from "./Links";
 
 export default function Sidebar() {
   return (
-    <aside className="hidden w-full max-w-[260px] space-y-2 md:block">
+    <aside className="hidden w-full max-w-[260px] space-y-2 px-4 md:block">
       <HeaderWrapper className="border-none" asDiv={true}>
         <Logo className="max-w-[100px]" />
       </HeaderWrapper>
