ccdfa2667b37c388454ee2d44781313c4f971ff9
empflow <empflow@tutanota.com>
2023-12-24 09:56:59 +0300

    add <HeaderAvatar />

---
+++ src/components/RootLayout/Header/Avatar.tsx
@@ -0,0 +1,41 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import useUsername from "@/lib/hooks/useUsername";
+import { signOut, useSession } from "next-auth/react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogOutIcon } from "lucide-react";
+
+export default function HeaderAvatar() {
+  const { status, data } = useSession();
+  const username = useUsername();
+  let avatarSrc = data?.user?.image ?? undefined;
+
+  if (status === "unauthenticated") return null;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src={avatarSrc} />
+          <AvatarFallback>{username.slice(0, 1)}</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-3 mr-1 min-w-[200px] max-w-[350px]">
+        <DropdownMenuLabel className="truncate">{username}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          className="space-x-2 cursor-pointer"
+          onClick={() => signOut()}
+        >
+          <LogOutIcon />
+          <span>Выйти</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
