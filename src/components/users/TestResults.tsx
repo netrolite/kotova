1c701a2a1ffe8caf19107ecf0e16ed1b8d1e77d6
empflow <empflow@tutanota.com>
2023-12-27 19:17:12 +0300

    add /users/[id] page

---
+++ src/components/users/TestResults.tsx
@@ -0,0 +1,31 @@
+import { Test, TestResult, User } from "@prisma/client";
+
+type Props = {
+  user: User;
+  testResults: (TestResult & { test: Test })[];
+};
+export default async function UserTestResults({ user, testResults }: Props) {
+  if (!testResults.length)
+    return (
+      <p className="text-muted-foreground">
+        Пользователь {user.name} ещё не прошел ни одного теста
+      </p>
+    );
+
+  return (
+    <ul>
+      {testResults.map((result) => (
+        <li key={result.id}>
+          <h3>{result.test.title}</h3>
+          {/* TODO: add subjects and grades */}
+          <div>
+            <span>Баллы:</span>
+            <span>
+              {result.score}/{result.test.maxScore} ({result.scorePercentage}%)
+            </span>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
