efb06764939b83889caeaaebb65c3bdcd5aef47f
empflow <empflow@tutanota.com>
2023-12-29 14:20:24 +0300

    add editOwnProfileAction

---
+++ src/lib/actions/editOwnProfile.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import { auth } from "@/auth";
+import { db } from "../db";
+import ServerActionReturn from "../types/ServerActionReturn";
+import UserEditSchema from "../zod/schemas/UserEdit";
+import { revalidatePath } from "next/cache";
+
+export default async function editOwnProfileAction(
+  data: unknown,
+): Promise<ServerActionReturn> {
+  const session = await auth();
+  const validationResult = UserEditSchema.safeParse(data);
+  if (!validationResult.success || !session?.user) return { error: true };
+  const {
+    data: { avatarUrl: image, ...profileData },
+  } = validationResult;
+
+  await db.user.update({
+    where: { id: session.user.id },
+    data: { ...profileData, image },
+  });
+
+  revalidatePath(`/users/${session.user.id}`);
+  return { data: true };
+}
