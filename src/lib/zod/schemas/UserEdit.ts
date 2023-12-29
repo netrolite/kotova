a78799dc83657ba2cd7950e1a030c5693487106f
empflow <empflow@tutanota.com>
2023-12-29 11:59:05 +0300

    fix UserEdit zod schema

--- src/lib/zod/schemas/UserEdit.ts
+++ src/lib/zod/schemas/UserEdit.ts
@@ -1,3 +1,4 @@
+import urlRegex from "@/lib/regexes/url";
 import { z } from "zod";
 const NAME_MIN_LEN = 3;
 const NAME_MAX_WORDS = 5;
@@ -14,38 +15,39 @@ const UserEditSchema = z.object({
     .max(NAME_MAX_LEN, `Имя должно быть не длиннее ${NAME_MAX_LEN} символов`)
     .refine(
       (data) => data.split(" ").length <= NAME_MAX_WORDS,
-      `Имя должно иметь не длиннее ${NAME_MAX_WORDS} слов`,
+      `Имя должно быть не длиннее ${NAME_MAX_WORDS} слов`,
     ),
   email: z
     .string()
     .email("Неверный формат электронной почты")
-    .min(3)
     .max(
       EMAIL_MAX_LEN,
       `Электронная почта должна быть не длиннее ${EMAIL_MAX_LEN} символов`,
     ),
   phone: z
     .string()
-    .transform((data) => data.replaceAll(/\D/, ""))
-    .pipe(
-      z
-        .string()
-        .min(
-          PHONE_MIN_LEN,
-          `Номер телефона должен быть не короче ${PHONE_MIN_LEN} символов (не учитывая любые символы, кроме цифр)`,
-        )
-        .max(
-          PHONE_MAX_LEN,
-          `Номер телефона должен быть не длиннее ${PHONE_MAX_LEN} символов (не учитывая любые символы, кроме цифр)`,
-        ),
+    .transform((data) => data.replaceAll(/\D/g, ""))
+    .refine(
+      (data) => data === "" || data.length >= PHONE_MIN_LEN,
+      `Номер телефона должен быть не короче ${PHONE_MIN_LEN} символов (не учитывая любые символы, кроме цифр)`,
+    )
+    .refine(
+      (data) => data === "" || data.length <= PHONE_MAX_LEN,
+      `Номер телефона должен быть не длиннее ${PHONE_MAX_LEN} символов (не учитывая любые символы, кроме цифр)`,
     ),
   avatarUrl: z
     .string()
-    .url("Ссылка на аватар должна быть рабочей ссылкой на изображение")
+    .regex(
+      urlRegex,
+      "Ссылка на аватар должна быть рабочей ссылкой на изображение",
+    )
     .max(
       AVATAR_URL_MAX_LEN,
       `Ссылка на аватар должна быть не длиннее ${AVATAR_URL_MAX_LEN} символов`,
     ),
 });
+
+export type UserEditSchemaType = z.infer<typeof UserEditSchema>;
+export type UserEditSchemaInputType = z.input<typeof UserEditSchema>;
 
 export default UserEditSchema;
