c0158d03f8827028662c78ca820a32de16fadae7
empflow <empflow@tutanota.com>
2023-12-29 14:19:55 +0300

    add loading spinner component

---
+++ src/lib/redirectToSignIn.ts
@@ -0,0 +1,9 @@
+import { headers as getHeaders } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default function redirectToSignIn() {
+  const headers = getHeaders();
+  const callbackUrl = headers.get("referer") || "/";
+  const redirectTo = `/api/auth/signin?callbackUrl=${callbackUrl}`;
+  return redirect(redirectTo);
+}
