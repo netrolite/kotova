4b6d6641edf76c058ddd6e7159a07f2cfd78f0bd
empflow <empflow@tutanota.com>
2023-12-29 14:31:09 +0300

    only conditionally show link to /my/classes

--- src/components/routes/layouts/root/Sidebar/Sidebar.tsx
+++ src/components/routes/layouts/root/Sidebar/Sidebar.tsx
@@ -1,14 +1,17 @@
 import Logo from "@/components/Logo";
 import HeaderWrapper from "../Header/Wrapper";
 import SidebarLinks from "./Links";
+import { auth } from "@/auth";
 
-export default function Sidebar() {
+export default async function Sidebar() {
+  const session = await auth();
+  const role = session ? (session?.user as any).role || null : null;
   return (
     <aside className="hidden w-full max-w-[260px] space-y-2 px-4 md:block">
       <HeaderWrapper className="border-none" asDiv={true}>
         <Logo className="max-w-[100px]" />
       </HeaderWrapper>
-      <SidebarLinks />
+      <SidebarLinks userRole={role} />
     </aside>
   );
 }
