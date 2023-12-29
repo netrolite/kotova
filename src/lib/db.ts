b2b60f41340fd3de714c6583494af583ddca8528
empflow <empflow@tutanota.com>
2023-12-29 18:24:22 +0300

    add prisma accelerate support

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 // prevents having too many open connections in the connection pool
 // in development due to hot reloading
-export const db = globalForPrisma.prisma || new PrismaClient();
+export const db =
+  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
