f4a1c54ce8af2638f9cc3ded7724ca45ee233990
empflow <empflow@tutanota.com>
2023-12-30 18:22:51 +0300

    Update ResponsiveDialog.tsx

--- src/components/ResponsiveDialog.tsx
+++ src/components/ResponsiveDialog.tsx
@@ -21,12 +21,14 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "./ui/button";
+import { cn } from "@/lib/shadcnUtils";
 
 type Props = {
   children: ReactNode;
   trigger: ReactNode;
   description?: string;
   title?: string;
+  drawerChildrenContainerClassName?: string;
 };
 
 export default function ResponsiveDialog({
@@ -34,10 +36,12 @@ export default function ResponsiveDialog({
   trigger,
   title,
   description,
+  drawerChildrenContainerClassName,
 }: Props) {
   const [open, setOpen] = useState(false);
   const { width } = useWindowSize();
   const [isDesktop, setIsDesktop] = useState(false);
+  const shouldShowHeader = title || description;
 
   useEffect(() => {
     if (!width) return;
@@ -50,10 +54,14 @@ export default function ResponsiveDialog({
       <Dialog open={open} onOpenChange={setOpen}>
         <DialogTrigger asChild>{trigger}</DialogTrigger>
         <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>{title}</DialogTitle>
-            <DialogDescription>{description}</DialogDescription>
-          </DialogHeader>
+          {shouldShowHeader && (
+            <DialogHeader>
+              {title && <DialogTitle>{title}</DialogTitle>}
+              {description && (
+                <DialogDescription>{description}</DialogDescription>
+              )}
+            </DialogHeader>
+          )}
           {children}
         </DialogContent>
       </Dialog>
@@ -64,11 +72,17 @@ export default function ResponsiveDialog({
     <Drawer open={open} onOpenChange={setOpen}>
       <DrawerTrigger asChild>{trigger}</DrawerTrigger>
       <DrawerContent>
-        <DrawerHeader className="text-left">
-          <DrawerTitle>{title}</DrawerTitle>
-          <DrawerDescription>{description}</DrawerDescription>
-        </DrawerHeader>
-        {children}
+        {shouldShowHeader && (
+          <DrawerHeader className="text-left">
+            {title && <DrawerTitle>{title}</DrawerTitle>}
+            {description && (
+              <DrawerDescription>{description}</DrawerDescription>
+            )}
+          </DrawerHeader>
+        )}
+        <div className={cn("p-4", drawerChildrenContainerClassName)}>
+          {children}
+        </div>
         <DrawerFooter className="pt-2">
           <DrawerClose asChild>
             <Button variant="outline">Отменить</Button>
