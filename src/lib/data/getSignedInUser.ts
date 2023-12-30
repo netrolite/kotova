bf0b217c07c43c2bd5d7a8ea94da839d716bdc9b
empflow <empflow@tutanota.com>
2023-12-30 16:58:06 +0300

    add `select` option to user fetching functions

--- src/lib/data/getSignedInUser.ts
+++ src/lib/data/getSignedInUser.ts
@@ -1,8 +1,12 @@
 import { auth } from "@/auth";
 import { db } from "../db";
+import { Prisma } from "@prisma/client";
 
-export default async function getSignedInUser() {
+export default async function getSignedInUser(userSelect?: Prisma.UserSelect) {
   const session = await auth();
   if (!session) return null;
-  return db.user.findFirst({ where: { id: session?.user?.id } });
+  return db.user.findFirst({
+    where: { id: session?.user?.id },
+    select: userSelect,
+  });
 }
--- src/lib/data/getSignedInUserOrRedirect.ts
+++ src/lib/data/getSignedInUserOrRedirect.ts
@@ -1,10 +1,16 @@
 import { db } from "../db";
 import authOrRedirect from "../authOrRedirect";
 import redirectToSignIn from "../redirectToSignIn";
+import { Prisma } from "@prisma/client";
 
-export default async function getSignedInUserOrRedirect() {
+export default async function getSignedInUserOrRedirect(
+  userSelect?: Prisma.UserSelect,
+) {
   const session = await authOrRedirect();
-  const user = await db.user.findFirst({ where: { id: session?.user?.id } });
+  const user = await db.user.findFirst({
+    where: { id: session?.user?.id },
+    select: userSelect,
+  });
   if (!user) return redirectToSignIn();
   return user;
 }
