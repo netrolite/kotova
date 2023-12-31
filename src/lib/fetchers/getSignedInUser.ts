7de38887685e4ff29435feb0b068e52998747896
empflow <empflow@tutanota.com>
2023-12-31 10:02:44 +0300

    cache data fetching functions

--- src/lib/fetchers/getSignedInUser.ts
+++ src/lib/fetchers/getSignedInUser.ts
@@ -1,12 +1,15 @@
 import { auth } from "@/auth";
 import { db } from "../db";
 import { Prisma } from "@prisma/client";
+import { cache } from "react";
 
-export default async function getSignedInUser(userSelect?: Prisma.UserSelect) {
+const getSignedInUser = cache(async (userSelect?: Prisma.UserSelect) => {
   const session = await auth();
   if (!session) return null;
   return db.user.findFirst({
     where: { id: session?.user?.id },
     select: userSelect,
   });
-}
+});
+
+export default getSignedInUser;
