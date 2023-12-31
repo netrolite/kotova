e76799a558105507b7616e5b9a00ad53640cca08
empflow <empflow@tutanota.com>
2023-12-31 09:21:22 +0300

    remove dumb (stupid and unnecessary) logic

--- src/app/tests/page.tsx
+++ src/app/tests/page.tsx
@@ -1,9 +1,6 @@
 import PageTitle from "@/components/PageTitle";
-import AddSubjectBtn from "@/components/routes/tests/AddSubject/Btn";
 import SubjectsList from "@/components/routes/tests/SubjectsList/SubjectsList";
 import { db } from "@/lib/db";
-import getSignedInUser from "@/lib/fetchers/getSignedInUser";
-import { ROLE } from "@/lib/types/enums/Role";
 
 export const metadata = {
   title: "Список тестов по предметам",
@@ -12,23 +9,16 @@ export const metadata = {
 };
 
 export default async function Tests() {
-  const signedInUserPromise = getSignedInUser({ role: true });
-  const subjectsPromise = db.subject.findMany({
+  const subjects = await db.subject.findMany({
     include: { tests: { include: { _count: true } } },
   });
-  const [signedInUser, subjects] = await Promise.all([
-    signedInUserPromise,
-    subjectsPromise,
-  ]);
-  const isAdmin = signedInUser?.role === ROLE.ADMIN;
 
   return (
     <>
       <div className="mb-8 space-y-2">
         <PageTitle className="mb-0">Тесты по предметам</PageTitle>
-        {isAdmin && <AddSubjectBtn />}
       </div>
-      <SubjectsList {...{ subjects, isAdmin }} />
+      <SubjectsList {...{ subjects }} />
     </>
   );
 }
