a39ec1101d6d2ec67bf927aee76f85e52e7382b5
empflow <empflow@tutanota.com>
2023-12-31 20:40:40 +0300

    remove wait() calls

--- src/lib/fetchers/getApiData.ts
+++ src/lib/fetchers/getApiData.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 import { cache } from "react";
-import wait from "../wait";
 
 const getApiData = cache(async (url: string) => {
-  await wait(1000);
   return (await axios.get(url)).data;
 });
 
