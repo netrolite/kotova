bc02f3f670f64cbdc4dc2f11b4c4c11f2da89383
empflow <empflow@tutanota.com>
2024-01-01 10:25:35 +0300

    Revert "refactor: rename constant"
    This reverts commit 6ceb1c7c0e87d2af24dae0540b5857eceaf394e8.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,2 @@
 export const genericErrorMsg = "Что-то пошло не так. Попробуйте еще раз позже!";
 export const GET_TESTS_FOR_MY_TEST_LIST_TESTS_PER_PAGE = 10;
-export const SUBJECT_TESTS_TESTS_PER_PAGE = 30;
