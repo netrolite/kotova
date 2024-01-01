8446e8e86bd046e1f3d1832e5715f5c3f5996394
empflow <empflow@tutanota.com>
2024-01-01 10:11:08 +0300

    replace phin with fetch api :laughing:

--- src/lib/fetchers/getApiData.ts
+++ src/lib/fetchers/getApiData.ts
@@ -1,9 +1,7 @@
 import { cache } from "react";
-import p from "phin";
-import { Subject } from "@prisma/client";
 
 const getApiData = cache(async (url: string) => {
-  return (await p<Subject[]>({ url, parse: "json" })).body;
+  return (await fetch(url)).json();
 });
 
 export default getApiData;
