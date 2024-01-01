8db0e0b5269a0de3c23d191a025f769043bd9ee1
empflow <empflow@tutanota.com>
2024-01-01 09:40:20 +0300

    replace axios with phin

--- src/lib/fetchers/getApiData.ts
+++ src/lib/fetchers/getApiData.ts
@@ -1,8 +1,9 @@
-import axios from "axios";
 import { cache } from "react";
+import p from "phin";
+import { Subject } from "@prisma/client";
 
 const getApiData = cache(async (url: string) => {
-  return (await axios.get(url)).data;
+  return (await p<Subject[]>({ url, parse: "json" })).body;
 });
 
 export default getApiData;
