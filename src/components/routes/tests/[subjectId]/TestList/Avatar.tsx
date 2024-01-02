e14dfdfdad1adb8f7c847fb8da6dccf78b46b97d
empflow <empflow@tutanota.com>
2024-01-02 13:19:05 +0300

    fix types

--- src/components/routes/tests/[subjectId]/TestList/Avatar.tsx
+++ src/components/routes/tests/[subjectId]/TestList/Avatar.tsx
@@ -2,9 +2,10 @@ import AvatarWithFallback from "@/components/AvatarWithFallback";
 import { User } from "@prisma/client";
 
 type Props = {
-  user: User;
+  user: User | null;
 };
 
-export default function SubjectTestListTestAvatar({ user: { image } }: Props) {
+export default function SubjectTestListTestAvatar({ user }: Props) {
+  const { image } = user || {};
   return <AvatarWithFallback width={24} src={image || undefined} />;
 }
