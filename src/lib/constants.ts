ae98ec8c50487ab49e5be5ac3113ca80240d08cc
empflow <empflow@tutanota.com>
2024-01-02 12:20:50 +0300

    show 30 tests per page on /my/tests

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 export const genericErrorMsg = "Что-то пошло не так. Попробуйте еще раз позже!";
-export const MY_TESTS_LIST_TESTS_PER_PAGE = 10;
+export const MY_TESTS_LIST_TESTS_PER_PAGE = 30;
 export const SUBJECT_TESTS_TESTS_PER_PAGE = 30;
