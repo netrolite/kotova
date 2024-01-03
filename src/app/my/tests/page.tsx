f6a0b2622e50108a1764d80966d024d6d2d8dc32
empflow <empflow@tutanota.com>
2024-01-03 13:17:48 +0300

    add link to /my/tests/add on /my/tests

--- src/app/my/tests/page.tsx
+++ src/app/my/tests/page.tsx
@@ -1,8 +1,11 @@
+import BtnWithIcon from "@/components/Btns/WithIcon";
 import PageTitle from "@/components/PageTitle";
 import MyTestsTestListInfiniteScroll from "@/components/routes/my/tests/TestList/InfiniteScroll";
 import MyTestsTestListTest from "@/components/routes/my/tests/TestList/Test";
 import getSignedInUserOrRedirect from "@/lib/fetchers/getSignedInUserOrRedirect";
 import getTestsForMyTestsList from "@/lib/fetchers/getTestsForMyTestsList";
+import { PlusIcon } from "lucide-react";
+import Link from "next/link";
 
 export default async function Tests() {
   const user = await getSignedInUserOrRedirect();
@@ -10,7 +13,12 @@ export default async function Tests() {
 
   return (
     <>
-      <PageTitle className="mb-8">Ваши тесты</PageTitle>
+      <div className="mb-8">
+        <PageTitle className="mb-2">Ваши тесты</PageTitle>
+        <Link href="/my/tests/add">
+          <BtnWithIcon icon={<PlusIcon />}>Добавить тест</BtnWithIcon>
+        </Link>
+      </div>
       <section>
         <ul className="space-y-4">
           {tests.map((test) => (
