376389de580b62810c2d629977406fb83175df4f
empflow <empflow@tutanota.com>
2024-01-04 15:09:47 +0300

    add 2 field to form at /my/tests/add

---
+++ src/components/routes/my/tests/add/Form/Index.tsx
@@ -0,0 +1,174 @@
+"use client";
+
+import { ComboboxItem } from "@/components/Combobox";
+import { grades as allGrades } from "@/lib/constants";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import useLoading from "@/lib/hooks/useLoading";
+import AddTestSchema, {
+  AddTestSchemaInputType,
+  AddTestSchemaType,
+} from "@/lib/zod/schemas/AddTest";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ControllerRenderProps, FormProvider, useForm } from "react-hook-form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Checkbox } from "@/components/ui/checkbox";
+import FormItemField from "@/components/Form/ItemField";
+import { CheckedState } from "@radix-ui/react-checkbox";
+
+type Props = {
+  subjects: ComboboxItem<string>[];
+};
+
+type GetGradesFieldPropParams<T = false> = {
+  currentGrade: number;
+  field: ControllerRenderProps<AddTestSchemaType, "grades">;
+  isLastInArray: boolean;
+  isChecked: T extends true ? CheckedState : undefined;
+};
+
+export default function AddTestForm({ subjects }: Props) {
+  const form = useForm<AddTestSchemaInputType>({
+    resolver: zodResolver(AddTestSchema),
+    defaultValues: {
+      grades: [],
+      name: undefined,
+      questions: [],
+      subject: undefined,
+    },
+  });
+  const { control } = form;
+  const { grades } = form.watch();
+  const { isLoading, setIsLoading } = useLoading();
+  // const [data, setData] = useState<Awaited<
+  //   ReturnType<typeof editOwnProfileAction>
+  // > | null>(null);
+
+  async function handleSubmit(data: AddTestSchemaType) {
+    setIsLoading(true);
+    // setData(await action(data));
+    setIsLoading(false);
+  }
+
+  function getGradesFieldIsChecked({
+    currentGrade: grade,
+    field,
+    isLastInArray,
+  }: GetGradesFieldPropParams) {
+    if (!isLastInArray) {
+      return field.value?.includes(grade);
+    }
+    return allGrades.every((grade) => grades.includes(grade));
+  }
+
+  function getGradesFieldOnCheckedChange({
+    currentGrade,
+    field,
+    isLastInArray,
+    isChecked,
+  }: GetGradesFieldPropParams<true>) {
+    if (!isLastInArray) {
+      if (isChecked) return field.onChange([...field.value, currentGrade]);
+      return field.onChange(
+        field.value?.filter((value) => value !== currentGrade),
+      );
+    }
+    if (isChecked) return form.setValue("grades", allGrades);
+    return form.setValue("grades", []);
+  }
+
+  return (
+    <FormProvider {...form}>
+      <form className="space-y-4" onSubmit={form.handleSubmit(handleSubmit)}>
+        <FormItemField
+          control={control}
+          name="subject"
+          render={({ field }) => (
+            <>
+              <FormLabel>Предмет</FormLabel>
+              <Select onValueChange={field.onChange}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Выберите предмет" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {subjects.map(({ label, value }) => (
+                    <SelectItem value={value.toString()}>{label}</SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </>
+          )}
+        />
+
+        <FormField
+          control={control}
+          name="grades"
+          render={() => (
+            <FormItem>
+              <div className="mb-4">
+                <FormLabel>Классы</FormLabel>
+                <FormDescription>
+                  Выберите классы, для которых предназначен этот тест
+                </FormDescription>
+              </div>
+              {/* grade 12 is rendered as a checkbox that selects all grades at once */}
+              {[...allGrades, 12].map((grade, i, arr) => {
+                const isLastInArray = i === arr.length - 1;
+                return (
+                  <FormField
+                    key={grade}
+                    control={control}
+                    name="grades"
+                    render={({ field }) => {
+                      return (
+                        <FormItem className="flex items-center gap-3 space-y-0">
+                          <FormControl>
+                            <Checkbox
+                              checked={getGradesFieldIsChecked({
+                                currentGrade: grade,
+                                field,
+                                isLastInArray,
+                                isChecked: undefined,
+                              })}
+                              onCheckedChange={(isChecked) =>
+                                getGradesFieldOnCheckedChange({
+                                  currentGrade: grade,
+                                  field,
+                                  isLastInArray,
+                                  isChecked,
+                                })
+                              }
+                            />
+                          </FormControl>
+                          <FormLabel className="text-sm font-normal">
+                            {isLastInArray ? "Все классы" : `${grade} класс`}
+                          </FormLabel>
+                        </FormItem>
+                      );
+                    }}
+                  />
+                );
+              })}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </FormProvider>
+  );
+}
