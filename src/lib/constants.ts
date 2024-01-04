f482b1dbc2fe9cc6335595e147f07ba1e2b57386
empflow <empflow@tutanota.com>
2024-01-04 15:04:38 +0300

    add `grades` constant
    just an array from 1 to 11

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,6 @@
+import { makeArr } from "./makeArr";
+
 export const genericErrorMsg = "Что-то пошло не так. Попробуйте еще раз позже!";
 export const MY_TESTS_LIST_TESTS_PER_PAGE = 30;
 export const SUBJECT_TESTS_TESTS_PER_PAGE = 30;
+export const grades = makeArr({ from: 1, to: 11 });
