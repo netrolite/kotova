2648db0d90fdbb59a62e02c8f14eba28d1855b97
empflow <empflow@tutanota.com>
2024-01-04 15:05:12 +0300

    add AddTest zod schema

---
+++ src/lib/zod/schemas/AddTest.ts
@@ -0,0 +1,38 @@
+import { z } from "zod";
+import { grades as allGrades } from "@/lib/constants";
+import { ROLE } from "@/lib/types/enums/Role";
+
+const NAME_MAX_LEN = 300;
+
+const AddTestSchema = z.object({
+  grades: z
+    .number({ required_error: "Не выбран класс или класс" })
+    .array()
+    .refine(
+      (selectedGrades) =>
+        selectedGrades.some((selectedGrade) =>
+          allGrades.some((grade) => grade === selectedGrade),
+        ),
+      { message: "Неверные классы" },
+    ),
+  subject: z
+    .string()
+    .min(1, { message: "Не выбран предмет" })
+    .cuid({ message: "Неверный формат предмета" }),
+  name: z
+    .string()
+    .min(1, "Имя не заполнено")
+    .max(NAME_MAX_LEN, `Имя должно быть не длиннее ${NAME_MAX_LEN} символов`),
+  questions: z.array(
+    z.object({
+      type: z
+        .number()
+        .refine((data) => (Object.values(ROLE) as number[]).includes(data)),
+    }),
+  ),
+});
+
+export type AddTestSchemaType = z.infer<typeof AddTestSchema>;
+export type AddTestSchemaInputType = z.input<typeof AddTestSchema>;
+
+export default AddTestSchema;
