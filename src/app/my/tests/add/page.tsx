0ed211ae2f2c25093cab81ca346c9c1a87dad0d7
empflow <empflow@tutanota.com>
2024-01-07 14:08:21 +0300

    fix errors

--- src/app/my/tests/add/page.tsx
+++ src/app/my/tests/add/page.tsx
@@ -1,11 +1,11 @@
-import { ComboboxItem } from "@/components/Combobox";
 import PageTitle from "@/components/PageTitle";
 import AddTestForm from "@/components/routes/my/tests/add/Form/Index";
 import getSubjects from "@/lib/fetchers/getSubjects";
+import SelectItemType from "@/lib/types/SelectItem";
 
 export default async function AddTest() {
   const subjectsRaw = await getSubjects();
-  const subjects: ComboboxItem<string>[] = subjectsRaw.map((subject) => ({
+  const subjects: SelectItemType<string>[] = subjectsRaw.map((subject) => ({
     label: subject.title,
     value: subject.id,
   }));
