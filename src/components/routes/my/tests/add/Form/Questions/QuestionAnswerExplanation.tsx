8b2fb3c9090587a7f2c59a5c67b7c5cb5a67da74
empflow <empflow@tutanota.com>
2024-01-09 13:46:10 +0300

    add label to answer explanation field

--- src/components/routes/my/tests/add/Form/Questions/QuestionAnswerExplanation.tsx
+++ src/components/routes/my/tests/add/Form/Questions/QuestionAnswerExplanation.tsx
@@ -1,6 +1,8 @@
-import { Controller, useFormContext } from "react-hook-form";
+import { useFormContext } from "react-hook-form";
 import { Textarea } from "@/components/ui/textarea";
 import { AddTestFormSchemaType } from "@/lib/zod/schemas/addTestForm/Index";
+import FormItemField from "@/components/Form/ItemField";
+import { FormControl, FormLabel, FormMessage } from "@/components/ui/form";
 
 type Props = {
   index: number;
@@ -9,10 +11,18 @@ type Props = {
 export default function AddTestFormQuestionAnswerExplanation({ index }: Props) {
   const { control } = useFormContext<AddTestFormSchemaType>();
   return (
-    <Controller
+    <FormItemField
       name={`questions.${index}.explanation`}
       control={control}
-      render={({ field }) => <Textarea {...field} value={field.value || ""} />}
+      render={({ field }) => (
+        <>
+          <FormLabel>Объяснение (необязательно)</FormLabel>
+          <FormControl>
+            <Textarea {...field} value={field.value || ""} />
+          </FormControl>
+          <FormMessage />
+        </>
+      )}
     />
   );
 }
