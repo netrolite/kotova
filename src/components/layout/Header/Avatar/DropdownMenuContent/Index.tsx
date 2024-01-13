c0c752900ddcc29ec6c1c82c63d7d9a3ac545328
empflow <empflow@tutanota.com>
2024-01-13 18:28:30 +0300

    add link to profile from profile dropdown menu

--- src/components/layout/Header/Avatar/DropdownMenuContent/Index.tsx
+++ src/components/layout/Header/Avatar/DropdownMenuContent/Index.tsx
@@ -8,15 +8,24 @@ import {
 import signOutAction from "@/lib/actions/signOut";
 import { LogOutIcon } from "lucide-react";
 import HeaderAvatarDropdownMenuContentItem from "./Item";
+import Link from "next/link";
 
 type Props = {
   username: string;
+  userId: string;
 };
 
-export default function HeaderAvatarDropdownMenuContent({ username }: Props) {
+export default function HeaderAvatarDropdownMenuContent({
+  username,
+  userId,
+}: Props) {
   return (
     <DropdownMenuContent align="end" className="min-w-[200px] max-w-[350px]">
-      <DropdownMenuLabel className="truncate">{username}</DropdownMenuLabel>
+      <DropdownMenuLabel>
+        <Link className="hover:underline" href={`/users/${userId}/`}>
+          {username}
+        </Link>
+      </DropdownMenuLabel>
       <DropdownMenuSeparator />
       <HeaderAvatarDropdownMenuContentItem
         formAction={signOutAction}
