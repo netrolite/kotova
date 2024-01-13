b9fc9b3ff78a878d229e078136cce3faeef6ce7d
empflow <empflow@tutanota.com>
2024-01-13 18:06:53 +0300

    refactor

--- src/components/layout/Header/RightSection/Content.tsx
+++ src/components/layout/Header/RightSection/Content.tsx
@@ -1,13 +1,10 @@
 import { auth } from "@/auth";
 import HeaderSignInBtn from "../SignInBtn";
 import HeaderAvatar from "../Avatar/Index";
-import { SessionProvider } from "next-auth/react";
 
 export default async function HeaderRightSectionContent() {
   const session = await auth();
 
-  let content = <HeaderSignInBtn />;
-  if (session?.user?.id) content = <HeaderAvatar />;
-
-  return <SessionProvider session={session}>{content}</SessionProvider>;
+  if (session?.user?.id) return <HeaderAvatar />;
+  else return <HeaderSignInBtn />;
 }
