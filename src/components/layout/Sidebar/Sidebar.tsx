481c4f5503a32148a88b8b53d6714f5dbf00909e
empflow <empflow@tutanota.com>
2024-01-13 18:04:05 +0300

    fix build

--- src/components/layout/Sidebar/Sidebar.tsx
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -1,17 +1,14 @@
 import Logo from "@/components/Logo";
 import HeaderWrapper from "../Header/Wrapper";
 import SidebarLinks from "./NavItems";
-import { auth } from "@/auth";
 
 export default async function Sidebar() {
-  const session = await auth();
-  const role = session ? (session?.user as any).role || null : null;
   return (
     <aside className="hidden w-full max-w-[260px] space-y-2 px-4 md:block">
       <HeaderWrapper className="border-none" asDiv={true}>
         <Logo className="max-w-[100px]" />
       </HeaderWrapper>
-      <SidebarLinks userRole={role} />
+      <SidebarLinks />
     </aside>
   );
 }
