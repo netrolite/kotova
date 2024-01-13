9e884c9d06e0a6d9241c4715de0a948d3c179630
empflow <empflow@tutanota.com>
2024-01-13 18:35:05 +0300

    highlight active nav item on mobile

--- src/components/layout/MobileNav/NavItem.tsx
+++ src/components/layout/MobileNav/NavItem.tsx
@@ -1,10 +1,18 @@
+"use client";
+
 import Link from "next/link";
 import { NavItemType } from "./Index";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/shadcnUtils";
 
 export default function MobileNavItem({ href, icon, label }: NavItemType) {
+  const pathname = usePathname();
+  const isActive = href === pathname;
   return (
     <Link key={href} href={href}>
-      <li className="flex flex-col items-center">
+      <li
+        className={cn("flex flex-col items-center", isActive && "text-primary")}
+      >
         {icon}
         <p className="whitespace-nowrap text-center text-[0.8rem]">{label}</p>
       </li>
