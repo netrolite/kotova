f9b277419291c240609797a7c0a138f9fe885d84
empflow <empflow@tutanota.com>
2024-01-14 16:13:11 +0300

    Update Header.tsx

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -4,7 +4,7 @@ import HeaderRightSection from "./RightSection/Index";
 
 export default function Header() {
   return (
-    <HeaderWrapper className="fixed left-0 right-0 top-0 z-20 bg-white/80 backdrop-blur-md md:flex">
+    <HeaderWrapper className="fixed left-0 right-0 top-0 z-20 bg-white/80 backdrop-blur-md md:sticky">
       <HeaderLeftSection />
       <HeaderRightSection />
     </HeaderWrapper>
