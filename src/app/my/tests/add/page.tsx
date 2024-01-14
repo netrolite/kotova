29fa39524d06f234f0e2c74490357297d1352774
empflow <empflow@tutanota.com>
2024-01-14 16:32:47 +0300

    prevent accessing /my/tests/add page when not signed in

--- src/app/my/tests/add/page.tsx
+++ src/app/my/tests/add/page.tsx
@@ -1,10 +1,14 @@
 import PageTitle from "@/components/PageTitle";
 import AddTestForm from "@/components/dashboard/tests/add/Form/Index";
+import getSignedInUserOrRedirect from "@/lib/fetchers/getSignedInUserOrRedirect";
 import getSubjects from "@/lib/fetchers/getSubjects";
 import SelectItemType from "@/lib/types/SelectItem";
 
 export default async function AddTest() {
-  const subjectsRaw = await getSubjects();
+  const [subjectsRaw] = await Promise.all([
+    getSubjects(),
+    getSignedInUserOrRedirect(),
+  ]);
   const subjects: SelectItemType<string>[] = subjectsRaw.map((subject) => ({
     label: subject.title,
     value: subject.id,
