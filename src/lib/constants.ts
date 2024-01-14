d8259dd064365f462476f534102ad0357c7fcfbf
empflow <empflow@tutanota.com>
2024-01-14 20:22:09 +0300

    add dateFormatterReasonableDefaults constant

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,3 +4,10 @@ export const DASHBOARD_TESTS_LIST_TESTS_PER_PAGE = 30;
 export const SUBJECT_TESTS_TESTS_PER_PAGE = 30;
 export const ALL_GRADES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11] as const;
 export type Grade = (typeof ALL_GRADES)[number];
+export const dateFormatterReasonableDefaults = {
+  year: "numeric",
+  month: "short",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+} satisfies Intl.DateTimeFormatOptions;
