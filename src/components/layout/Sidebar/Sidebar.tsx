e34f9409d408e23f1572927f9e608824119828c1
empflow <empflow@tutanota.com>
2024-01-15 20:14:31 +0300

    make sidebar fixed

--- src/components/layout/Sidebar/Sidebar.tsx
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -4,7 +4,7 @@ import SidebarLinks from "./NavItems";
 
 export default async function Sidebar() {
   return (
-    <aside className="hidden w-full max-w-[260px] space-y-2 px-4 md:block">
+    <aside className="sticky top-0 hidden h-full w-full max-w-[260px] space-y-2 px-4 md:block">
       <HeaderWrapper className="border-none" asDiv={true}>
         <Logo className="max-w-[100px]" />
       </HeaderWrapper>
