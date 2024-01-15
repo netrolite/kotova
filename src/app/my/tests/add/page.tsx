94c3aed5f0a52f9cd91eff9c31136d751a7e7c9f
empflow <empflow@tutanota.com>
2024-01-15 19:44:56 +0300

    show access denied error if student tries to create test

--- src/app/my/tests/add/page.tsx
+++ src/app/my/tests/add/page.tsx
@@ -1,14 +1,17 @@
+import AccessDenied from "@/components/AccessDenied";
 import PageTitle from "@/components/PageTitle";
 import AddTestForm from "@/components/dashboard/tests/add/Form/Index";
 import getSignedInUserOrRedirect from "@/lib/fetchers/getSignedInUserOrRedirect";
 import getSubjects from "@/lib/fetchers/getSubjects";
 import SelectItemType from "@/lib/types/SelectItem";
+import { ROLE } from "@/lib/types/enums/Role";
 
 export default async function AddTest() {
-  const [subjectsRaw] = await Promise.all([
+  const [subjectsRaw, user] = await Promise.all([
     getSubjects(),
     getSignedInUserOrRedirect(),
   ]);
+  if (user.role !== ROLE.TEACHER) return <AccessDenied />;
   const subjects: SelectItemType<string>[] = subjectsRaw.map((subject) => ({
     label: subject.title,
     value: subject.id,
