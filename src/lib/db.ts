d35230cf3704ab0ed3752985eb3962ebbcbb32c7
empflow <empflow@tutanota.com>
2024-01-17 16:27:45 +0300

    remove prisma accelerate

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,10 @@
 import { PrismaClient } from "@prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
 import isProduction from "./isProduction";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 // prevents having too many open connections in the connection pool
 // in development due to hot reloading
-export const db =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+export const db = globalForPrisma.prisma || new PrismaClient();
 
 if (!isProduction()) globalForPrisma.prisma = db;
