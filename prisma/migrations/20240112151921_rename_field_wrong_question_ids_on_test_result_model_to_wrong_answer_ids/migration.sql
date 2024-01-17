3d43083cc0ea785fd2431652852e76cb24ce05cc
empflow <empflow@tutanota.com>
2024-01-17 16:24:14 +0300

    change db provider to postgres

--- prisma/migrations/20240112151921_rename_field_wrong_question_ids_on_test_result_model_to_wrong_answer_ids/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `wrongQuestionsIds` on the `TestResult` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "TestResult" DROP COLUMN "wrongQuestionsIds";
-ALTER TABLE "TestResult" ADD COLUMN     "wrongAnswerIds" STRING[];
--- prisma/migrations/20240112181829_rename_test_question_id_field_on_test_result_answer_mode_to_question_id/migration.sql
+++
@@ -1,2 +0,0 @@
--- RenameForeignKey
-ALTER TABLE "TestResultAnswer" RENAME CONSTRAINT "TestResultAnswer_testQuestionId_fkey" TO "TestResultAnswer_questionId_fkey";
