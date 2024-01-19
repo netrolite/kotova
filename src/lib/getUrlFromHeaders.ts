af05b904a7ba7f595f2293f62e212989f9e0f788
empflow <empflow@tutanota.com>
2024-01-19 18:48:37 +0300

    Create getUrlFromHeaders.ts

---
+++ src/lib/getUrlFromHeaders.ts
@@ -0,0 +1,5 @@
+import { headers as getHeaders } from "next/headers";
+export default function getUrlFromHeaders() {
+  const headers = getHeaders();
+  return headers.get("x-url") || headers.get("referer") || "/";
+}
