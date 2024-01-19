7660ba760957ab65ab17ba15606fb635886a9179
empflow <empflow@tutanota.com>
2024-01-19 17:10:36 +0300

    display score as percentage instead of score/100

--- src/components/userProfile/RecentTestResults/Result/Content.tsx
+++ src/components/userProfile/RecentTestResults/Result/Content.tsx
@@ -8,11 +8,7 @@ export default function UserProfileTestResultContent({}: Props) {
   return (
     <div className="flex gap-2">
       <span className="text-muted-foreground">Баллы</span>
-      <div className="flex gap-0.5 font-medium">
-        <span>{roundTestScore(score)}</span>
-        <span>/</span>
-        <span>100</span>
-      </div>
+      <span className="flex gap-0.5 font-medium">{roundTestScore(score)}%</span>
     </div>
   );
 }
