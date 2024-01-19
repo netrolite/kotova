795fc31dbde79980f8649c82b8f20644d064d607
empflow <empflow@tutanota.com>
2024-01-19 18:49:27 +0300

    append hideBackBtn parameter to url after sign in

--- src/lib/actions/signIn.ts
+++ src/lib/actions/signIn.ts
@@ -1,7 +1,14 @@
 "use server";
 
 import { signIn } from "@/auth";
+import getUrlFromHeaders from "../getUrlFromHeaders";
 
 export default async function signInAction() {
-  return signIn();
+  const callbackUrlFromHeaders = getUrlFromHeaders();
+  const callbackUrl = new URL(callbackUrlFromHeaders);
+  callbackUrl.searchParams.append("hideBackBtn", "");
+
+  return signIn(undefined, {
+    redirectTo: callbackUrl.toString(),
+  });
 }
