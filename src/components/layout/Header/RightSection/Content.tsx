16109cb3f6e82f0e6ad4c9444e4b4b8123d9fd74
empflow <empflow@tutanota.com>
2024-01-24 15:01:44 +0300

    fix not hiding back btn after sign up/in

--- src/components/layout/Header/RightSection/Content.tsx
+++ src/components/layout/Header/RightSection/Content.tsx
@@ -1,10 +1,12 @@
 import { auth } from "@/auth";
 import HeaderSignInBtn from "../SignInBtn";
 import HeaderAvatar from "../Avatar/Index";
+import getUrlFromHeaders from "@/lib/getUrlFromHeaders";
 
 export default async function HeaderRightSectionContent() {
   const session = await auth();
+  const url = getUrlFromHeaders() || "/";
 
   if (session?.user?.id) return <HeaderAvatar {...{ session }} />;
-  else return <HeaderSignInBtn />;
+  else return <HeaderSignInBtn {...{ url }} />;
 }
--- src/components/layout/Header/RightSection/Index.tsx
+++ src/components/layout/Header/RightSection/Index.tsx
@@ -1,11 +1,13 @@
 import { Suspense } from "react";
 import HeaderSignInBtn from "../SignInBtn";
 import HeaderRightSectionContent from "./Content";
+import getUrlFromHeaders from "@/lib/getUrlFromHeaders";
 
 export default function HeaderRightSection() {
+  const url = getUrlFromHeaders() || "/";
   return (
     <div className="ml-auto flex items-center">
-      <Suspense fallback={<HeaderSignInBtn />}>
+      <Suspense fallback={<HeaderSignInBtn {...{ url }} />}>
         <HeaderRightSectionContent />
       </Suspense>
     </div>
