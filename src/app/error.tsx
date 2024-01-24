e6a5a46884be3d0786858a7249f99e387c1e3dd5
empflow <empflow@tutanota.com>
2024-01-24 17:51:44 +0300

    fix not found & error pages

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,16 +1,20 @@
 "use client";
 
+import BackBtn from "@/components/BackBtn";
 import PageTitle from "@/components/PageTitle";
 import { Button } from "@/components/ui/button";
 import NextError from "@/lib/types/nextError";
 
 export default function Error({ reset }: NextError) {
   return (
-    <>
-      <PageTitle className="mb-4">Произошла неожиданная ошибка</PageTitle>
-      <Button onClick={reset} variant="outline">
-        Попробовать еще раз
-      </Button>
-    </>
+    <main className="m-auto max-w-[650px] px-4 py-8">
+      <PageTitle className="mb-6">Произошла неожиданная ошибка</PageTitle>
+      <div className="flex flex-col gap-2">
+        <Button onClick={reset} variant="outline" className="w-min">
+          Попробовать еще раз
+        </Button>
+        <BackBtn variant="outline" className="w-min" />
+      </div>
+    </main>
   );
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,13 @@
-"use client";
-
+import BackBtn from "@/components/BackBtn";
 import PageTitle from "@/components/PageTitle";
-import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
+import MainLayout from "./(main)/layout";
 
 export default function NotFound() {
-  const router = useRouter();
   return (
-    <main>
+    <MainLayout>
       <PageTitle>Страница не найдена</PageTitle>
       <p className="mb-4">Возможно, вы перешли по нерабочей ссылке.</p>
-      <Button onClick={router.back}>Вернуться назад</Button>
-    </main>
+      <BackBtn />
+    </MainLayout>
   );
 }
