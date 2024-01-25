f6328dd78f2e2fd5444260f13cf584d77796a2fd
empflow <empflow@tutanota.com>
2024-01-25 14:09:30 +0300

    refactor

--- src/components/dashboard/tests/add/Form/Questions/Index.tsx
+++ src/components/dashboard/tests/add/Form/Questions/Index.tsx
@@ -1,31 +1,22 @@
 import { AddTestFormQuestionSchemaType } from "@/lib/zod/schemas/addTestForm/Question";
 import { AddTestFormQuestions } from "../Index";
 import useAddTestFormContext from "@/lib/hooks/addTestForm/context";
-import FormError from "@/components/Form/Error";
 import AddTestFormQuestionList from "./List";
-import { Controller } from "react-hook-form";
+import { FormField, FormMessage } from "@/components/ui/form";
 
 export type AddTestFormQuestionSchemaWithId = AddTestFormQuestionSchemaType & {
   id: string;
 };
 
 export default function AddTestFormQuestions() {
-  const {
-    formState: { errors },
-    control,
-  } = useAddTestFormContext();
-
+  const { control } = useAddTestFormContext();
   return (
     <>
       <h3 className="mb-2 text-xl font-semibold">Вопросы</h3>
-      <Controller
+      <FormField
         control={control}
         name="questions"
-        render={() => (
-          <FormError
-            error={errors.questions?.message || errors.questions?.root?.message}
-          />
-        )}
+        render={() => <FormMessage />}
       />
       <AddTestFormQuestionList />
     </>
