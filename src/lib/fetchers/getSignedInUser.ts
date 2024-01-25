48614b55eafcd68bb1defd00d9019c80ba59563b
empflow <empflow@tutanota.com>
2024-01-25 13:15:32 +0300

    properly type getSignedInUser()

--- src/lib/fetchers/getSignedInUser.ts
+++ src/lib/fetchers/getSignedInUser.ts
@@ -3,13 +3,18 @@ import { db } from "../db";
 import { Prisma } from "@prisma/client";
 import { cache } from "react";
 
-const getSignedInUser = cache(async (args?: Prisma.UserFindFirstArgs) => {
-  const session = await auth();
-  if (!session) return null;
-  return db.user.findFirst({
-    where: { id: session?.user?.id },
-    ...args,
-  });
-});
+const getSignedInUser = cache(
+  async <T extends Prisma.UserFindFirstArgs>(
+    args?: T,
+  ): Promise<Prisma.UserGetPayload<T> | null> => {
+    const session = await auth();
+    if (!session?.user?.id) return null;
+
+    return db.user.findFirst({
+      where: { id: session.user.id },
+      ...args,
+    }) as Promise<Prisma.UserGetPayload<T> | null>;
+  },
+);
 
 export default getSignedInUser;
