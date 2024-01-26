f66be2910d649a016956ac988d1c8cb4a9020b01
empflow <empflow@tutanota.com>
2024-01-26 19:09:30 +0300

    fix /take-test/[id] & /test-result/[id] links width

--- src/components/testResult/Metadata/Subject.tsx
+++ src/components/testResult/Metadata/Subject.tsx
@@ -5,10 +5,12 @@ import Link from "next/link";
 export default function TestResultMetadataSubject() {
   const testResult = useTestResultContext();
   return (
-    <Link className="w-min" href={`/subjects/${testResult.test.subject?.id}`}>
-      <h2 className="text-muted-foreground">
-        {testResult.test.subject?.title}
-      </h2>
-    </Link>
+    <div>
+      <Link href={`/subjects/${testResult.test.subject?.id}`}>
+        <h2 className="inline text-muted-foreground">
+          {testResult.test.subject?.title}
+        </h2>
+      </Link>
+    </div>
   );
 }
