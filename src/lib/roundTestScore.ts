b778cc9690e69f38acfa3fd276da568688f54332
empflow <empflow@tutanota.com>
2024-01-26 16:08:27 +0300

    fix roundTestScore() returning 5.0 instead of 5

--- src/lib/roundTestScore.ts
+++ src/lib/roundTestScore.ts
@@ -1,4 +1,13 @@
 export default function roundTestScore(score: number) {
   if (Number.isInteger(score)) return score;
+
+  const scoreRoundedToInt = Math.round(score);
+  const isDiffBetweenRoundedAndNormalScoreNegligible =
+    Math.abs(scoreRoundedToInt - score) <= 0.1;
+
+  if (isDiffBetweenRoundedAndNormalScoreNegligible) {
+    return scoreRoundedToInt;
+  }
+
   return score.toFixed(1);
 }
