d4d6418b3f306a767d3453668de7e4f030ac4885
empflow <empflow@tutanota.com>
2024-01-27 17:42:32 +0300

    add service worker

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+const withPwa = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -39,4 +44,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPwa(nextConfig);
