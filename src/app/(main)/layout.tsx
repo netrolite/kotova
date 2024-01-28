71430f135b6af30c584061a4cc8f8e9feb6d6e28
empflow <empflow@tutanota.com>
2024-01-28 15:52:06 +0300

    fix not enough top padding for main content container on ios pwa

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,7 +1,9 @@
 import MainLayoutHeader from "@/components/mainLayout/Header/Index";
 import MobileNav from "@/components/mainLayout/MobileNav/Index";
 import Sidebar from "@/components/mainLayout/Sidebar/Index";
+import { cn } from "@/lib/shadcnUtils";
 import { ReactNode } from "react";
+import styles from "./layout.module.scss";
 
 export default async function MainLayout({
   children,
@@ -13,7 +15,9 @@ export default async function MainLayout({
       <Sidebar />
       <main className="flex min-h-screen w-full flex-col md:max-w-[650px] md:border-l md:border-r md:border-slate-200">
         <MainLayoutHeader />
-        <div className="flex-grow px-4 py-24 md:py-8">{children}</div>
+        <div className={cn(styles.mainContentContainer, "flex-grow px-4")}>
+          {children}
+        </div>
         <MobileNav />
       </main>
     </div>
