655150b21c01cc8accf494b6d53937808487cc89
empflow <empflow@tutanota.com>
2024-01-28 19:10:09 +0300

    fix offline page icon alignment

--- src/app/(main)/~offline/page.tsx
+++ src/app/(main)/~offline/page.tsx
@@ -4,9 +4,9 @@ import { WifiOffIcon } from "lucide-react";
 export default async function OfflineFallback() {
   return (
     <>
-      <div className="flex gap-2">
+      <div className="flex items-center gap-4">
         <WifiOffIcon />
-        <PageTitle> Вы не в сети</PageTitle>
+        <PageTitle>Вы не в сети</PageTitle>
       </div>
     </>
   );
