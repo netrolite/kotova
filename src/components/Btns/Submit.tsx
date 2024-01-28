b58ae3c19b3e7cb857657bdb9510b07a857c0574
empflow <empflow@tutanota.com>
2024-01-28 16:40:38 +0300

    fix react refs-related error at /my/tests/create

--- src/components/Btns/Submit.tsx
+++ src/components/Btns/Submit.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, forwardRef } from "react";
 import { Button, ButtonProps } from "../ui/button";
 import Loading from "../Loading/Loading";
 
@@ -7,20 +7,21 @@ type Props = {
   isLoading?: boolean;
 } & ButtonProps;
 
-export default function FormSubmitBtn({
-  children,
-  isLoading,
-  ...restProps
-}: Props) {
-  return (
-    <Button
-      type="submit"
-      disabled={isLoading}
-      className="flex items-center gap-1"
-      {...restProps}
-    >
-      {isLoading && <Loading className="h-4 w-4 text-white" />}
-      <span>{children}</span>
-    </Button>
-  );
-}
+const FormSubmitBtn = forwardRef<HTMLButtonElement, Props>(
+  ({ children, isLoading, ...restProps }, ref) => {
+    return (
+      <Button
+        type="submit"
+        disabled={isLoading}
+        className="flex items-center gap-1"
+        ref={ref}
+        {...restProps}
+      >
+        {isLoading && <Loading className="h-4 w-4 text-white" />}
+        <span>{children}</span>
+      </Button>
+    );
+  },
+);
+
+export default FormSubmitBtn;
