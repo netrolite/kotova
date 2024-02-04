7722d6c83837df9c90b7ffb7f97d87d59f476a6f
empflow <empflow@tutanota.com>
2024-02-04 11:47:28 +0300

    add safe-area-inset-top padding to auth layout

--- src/components/authLayout/Header.tsx
+++ src/components/authLayout/Header.tsx
@@ -1,11 +1,13 @@
+import { cn } from "@/lib/shadcnUtils";
 import Logo from "../Logo";
+import styles from "./styles.module.scss";
 
 type Props = {};
 
 export default function AuthLayoutHeader({}: Props) {
   return (
-    <div className="m-auto mb-6 max-w-[120px]">
+    <header className={cn("m-auto mb-6 max-w-[120px]", styles.header)}>
       <Logo />
-    </div>
+    </header>
   );
 }
