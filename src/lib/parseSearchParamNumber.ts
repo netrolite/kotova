9b5902f8a512156a907cdd9bd1cbad0958b31d43
empflow <67281212+empflow@users.noreply.github.com>
2024-02-10 15:25:36 +0300

    /my/tests/[id] show score filter min & max values

--- src/lib/parseSearchParamNumber.ts
+++ src/lib/parseSearchParamNumber.ts
@@ -1,4 +1,4 @@
-export default function parseSearchParamNumber(val?: string) {
+export default function parseSearchParamNumber(val?: string | null) {
   const parsed = Number(val);
   if (isNaN(parsed)) return undefined;
   return parsed;
