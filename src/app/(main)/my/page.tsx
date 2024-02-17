6b433b1777260aa164f65e4fba653f429a0f74c6
netrolite <67281212+netrolite@users.noreply.github.com>
2024-02-17 08:22:02 +0300

    add more dashboard cards

--- src/app/(main)/my/page.tsx
+++ src/app/(main)/my/page.tsx
@@ -2,24 +2,46 @@ import PageTitle from "@/components/PageTitle";
 import DashboardCardWrapper from "@/components/dashboard/Card/Wrapper";
 import DashboardAvgScoreCard from "@/components/dashboard/Cards/AvgScore";
 import DashboardSubjectsCard from "@/components/dashboard/Cards/Subjects";
-import DashboardTestsCard from "@/components/dashboard/Cards/Tests";
+import DashboardTakenTestsCard from "@/components/dashboard/Cards/TakenTests";
+import DashboardMyTestsCard from "@/components/dashboard/Cards/MyTests";
+import getSignedInUserOrRedirect from "@/lib/fetchers/getSignedInUserOrRedirect";
+import { ROLE } from "@/lib/types/enums/Role";
 
 export default async function Dashboard() {
+  const user = await getSignedInUserOrRedirect();
+  const isAdmin = user.role === ROLE.ADMIN;
+  const isTeacher = user.role === ROLE.TEACHER;
+  const isStudent = user.role === ROLE.STUDENT;
+
   return (
     <>
       <PageTitle className="mb-8">Личный кабинет</PageTitle>
       <section className="grid grid-cols-1 gap-4 lg:grid-cols-2">
-        <DashboardCardWrapper>
-          <DashboardTestsCard />
-        </DashboardCardWrapper>
+        {isAdmin ||
+          (isTeacher && (
+            <DashboardCardWrapper>
+              <DashboardMyTestsCard />
+            </DashboardCardWrapper>
+          ))}
 
-        <DashboardCardWrapper>
-          <DashboardSubjectsCard />
-        </DashboardCardWrapper>
+        {isAdmin && (
+          <DashboardCardWrapper>
+            <DashboardSubjectsCard />
+          </DashboardCardWrapper>
+        )}
 
-        <DashboardCardWrapper>
-          <DashboardAvgScoreCard />
-        </DashboardCardWrapper>
+        {isStudent ||
+          (isAdmin && (
+            <>
+              <DashboardCardWrapper>
+                <DashboardAvgScoreCard />
+              </DashboardCardWrapper>
+
+              <DashboardCardWrapper>
+                <DashboardTakenTestsCard />
+              </DashboardCardWrapper>
+            </>
+          ))}
       </section>
     </>
   );
--- src/components/dashboard/Cards/MyTests.tsx
+++ src/components/dashboard/Cards/MyTests.tsx
@@ -3,7 +3,7 @@ import DashboardCard from "../Card/Card";
 import getTestsCount from "@/lib/fetchers/getTestsCount";
 import { ROLE, Role } from "@/lib/types/enums/Role";
 
-export default async function DashboardTestsCard() {
+export default async function DashboardMyTestsCard() {
   const user = await getSignedInUserOrRedirect();
   const allowedRoles: Role[] = [ROLE.ADMIN, ROLE.TEACHER];
   if (!allowedRoles.includes(user.role as Role)) return null;
