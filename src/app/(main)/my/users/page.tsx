544735423a9271d1bbcee5b25dad521f486193c7
netrolite <67281212+netrolite@users.noreply.github.com>
2024-02-18 16:31:15 +0300

    only allow admins at /my/users

--- src/app/(main)/my/users/page.tsx
+++ src/app/(main)/my/users/page.tsx
@@ -5,8 +5,14 @@ import AllUsersSearch from "@/components/allUsers/Search";
 import { unstable_serialize as unstable_serializeInfinite } from "swr/infinite";
 import getUsers from "@/lib/fetchers/allUsers/getUsers";
 import allUsersGetKey from "@/lib/fetchers/allUsers/getKey";
+import getSignedInUserOrRedirect from "@/lib/fetchers/getSignedInUserOrRedirect";
+import { ROLE } from "@/lib/types/enums/Role";
+import AccessDenied from "@/components/AccessDenied";
 
 export default async function AllUsers() {
+  const signedInUser = await getSignedInUserOrRedirect();
+  if (signedInUser.role !== ROLE.ADMIN) return <AccessDenied />;
+
   const users = await getUsers({ page: 0 });
 
   return (
