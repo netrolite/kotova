296c824e3e77664dc82d74be150d6cbef77a4115
netrolite <67281212+netrolite@users.noreply.github.com>
2024-02-25 12:07:54 +0300

    fix not loggin ips

--- src/app/api/log-ip/route.ts
+++ src/app/api/log-ip/route.ts
@@ -1,15 +1,18 @@
 import { db } from "@/lib/db";
 import getSignedInUser from "@/lib/fetchers/getSignedInUser";
-import { NextRequest, NextResponse } from "next/server";
+import { NextApiRequest } from "next";
+import { NextResponse } from "next/server";
+import requestIp from "request-ip";
 
-export async function GET(req: NextRequest) {
+export async function GET(req: NextApiRequest) {
   const user = await getSignedInUser({ select: { id: true, ips: true } });
   if (!user) return NextResponse.json({ ok: true });
 
+  const currIp = requestIp.getClientIp(req);
   const existingIps = user.ips.map((ip) => ip.ip);
-  if (req.ip && !existingIps.includes(req.ip)) {
+  if (currIp && !existingIps.includes(currIp)) {
     await db.ip.create({
-      data: { ip: req.ip, user: { connect: { id: user.id } } },
+      data: { ip: currIp, user: { connect: { id: user.id } } },
     });
   }
 
