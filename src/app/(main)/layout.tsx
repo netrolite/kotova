efda1859ecfcac539f2092eeb101306cb31610bd
netrolite <netrolite@proton.me>
2024-04-02 14:19:01 +0300

    refactor

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,6 +3,8 @@ import Sidebar from "@/components/mainLayout/Sidebar/Index";
 import { cn } from "@/lib/shadcnUtils";
 import { ReactNode } from "react";
 import styles from "./layout.module.scss";
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth";
 
 export default async function MainLayout({
   children,
@@ -10,14 +12,16 @@ export default async function MainLayout({
   children: ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen w-full justify-center">
-      <Sidebar />
-      <main className="flex w-full flex-col md:max-w-[650px] md:border-l md:border-r md:border-slate-200">
-        <MainLayoutHeader />
-        <div className={cn(styles.mainContentContainer, "flex-grow px-4")}>
-          {children}
-        </div>
-      </main>
-    </div>
+    <SessionProvider session={await auth()}>
+      <div className="flex min-h-screen w-full justify-center">
+        <Sidebar />
+        <main className="flex w-full flex-col md:max-w-[650px] md:border-l md:border-r md:border-slate-200">
+          <MainLayoutHeader />
+          <div className={cn(styles.mainContentContainer, "flex-grow px-4")}>
+            {children}
+          </div>
+        </main>
+      </div>
+    </SessionProvider>
   );
 }
