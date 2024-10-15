7b9ac20b5225f802344e4fcea78779a32bea8259
netrolite <67281212+netrolite@users.noreply.github.com>
2024-10-15 14:07:14 +0300

    add teacher's profile image

--- src/components/dashboard/about/ProfileImg.tsx
+++ src/components/dashboard/about/ProfileImg.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import ProfileImg from "../../../../images/certificate1.webp";
+import ProfileImg from "../../../../images/profileImg.jpeg";
 import Zoom from "react-medium-image-zoom-fixed";
 import Image from "next/image";
 
