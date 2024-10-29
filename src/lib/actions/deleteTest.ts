73a229194ac66097b7c6dccf1058f5dfe790fa73
netrolite <67281212+netrolite@users.noreply.github.com>
2024-10-29 10:03:59 +0300

    fix test deletion not working

--- src/lib/actions/deleteTest.ts
+++ src/lib/actions/deleteTest.ts
@@ -5,10 +5,15 @@ import { revalidatePath } from "next/cache";
 import { db } from "../db";
 import ServerActionReturn from "../types/ServerActionReturn";
 import DeleteTestSchema from "../zod/schemas/deleteTest";
+import s3 from "../s3";
+import S3 from "aws-sdk/clients/s3";
+import getEnvVar from "../getEnvVar";
 
 export default async function deleteTestAction(
   data: unknown,
-): Promise<ServerActionReturn<true, string>> {
+): Promise<ServerActionReturn<true, string | boolean>> {
+  console.log("delete test");
+
   const session = await auth();
   if (!session?.user?.id) return { error: "no credentials provided" };
 
@@ -19,10 +24,23 @@ export default async function deleteTestAction(
   } = validationResult;
 
   try {
-    await db.test.delete({ where: { id: testId } });
+    const test = await db.test.delete({
+      where: { id: testId },
+      include: { files: true },
+    });
+    const params: S3.DeleteObjectsRequest = {
+      Bucket: getEnvVar("S3_BUCKET_NAME"),
+      Delete: {
+        Objects: test.files.map((file) => ({ Key: file.key })),
+      },
+    };
+    const filesDeletionResult = await s3.deleteObjects(params).promise();
+    console.log(filesDeletionResult);
+
     revalidatePath("/my/tests");
     return { data: true };
   } catch (err) {
-    return { error: "test not found" };
+    console.log(err);
+    return { error: true };
   }
 }
