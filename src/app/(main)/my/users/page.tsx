e7fd47531f97e8ffc6fdfbac8bd2cc64b75ba7df
netrolite <67281212+netrolite@users.noreply.github.com>
2024-11-03 20:40:44 +0300

    fix users page

--- src/app/(main)/my/users/page.tsx
+++ src/app/(main)/my/users/page.tsx
@@ -1,10 +1,6 @@
 import PageTitle from "@/components/PageTitle";
-import SwrProvider from "@/components/SwrProvider";
 import AllUsersList from "@/components/allUsers/List";
-import AllUsersSearch from "@/components/allUsers/Search";
-import { unstable_serialize as unstable_serializeInfinite } from "swr/infinite";
 import getUsers from "@/lib/fetchers/allUsers/getUsers";
-import allUsersGetKey from "@/lib/fetchers/allUsers/getKey";
 import getSignedInUserOrRedirect from "@/lib/fetchers/getSignedInUserOrRedirect";
 import { ROLE } from "@/lib/types/enums/Role";
 import AccessDenied from "@/components/AccessDenied";
@@ -12,24 +8,14 @@ import AccessDenied from "@/components/AccessDenied";
 export default async function AllUsers() {
   const signedInUser = await getSignedInUserOrRedirect();
   if (signedInUser.role !== ROLE.ADMIN) return <AccessDenied />;
-
-  const usersFirstPage = await getUsers({});
+  const users = await getUsers({});
 
   return (
     <>
       <PageTitle className="mb-6">Пользователи</PageTitle>
-      <SwrProvider
-        config={{
-          fallback: {
-            [unstable_serializeInfinite(allUsersGetKey)]: [usersFirstPage],
-          },
-        }}
-      >
-        <div className="space-y-6">
-          <AllUsersSearch />
-          <AllUsersList />
-        </div>
-      </SwrProvider>
+      <div className="space-y-6">
+        <AllUsersList users={users} />
+      </div>
     </>
   );
 }
