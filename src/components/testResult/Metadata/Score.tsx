c213f96956ebcc4123f289c6b518f73678a8b3c2
anonymous <netrolite@proton.me>
2025-01-29 13:12:36 +0300

    improve test result score display

--- src/components/testResult/Metadata/Score.tsx
+++ src/components/testResult/Metadata/Score.tsx
@@ -1,17 +1,21 @@
 "use client";
 import KeyValue from "@/components/KeyValue";
-import formatTestScore from "@/lib/formatTestScore";
 import useTestResultContext from "@/lib/hooks/testResult/context";
+import roundTestScore from "@/lib/roundTestScore";
 
 export default function TestResultMetadataScore() {
   const testResult = useTestResultContext();
+  const correctAnswers = testResult.answers.filter(answer => answer.isCorrect).length
+  const totalAnswers = testResult.answers.length
+
   return (
-    <KeyValue
-      label="Баллы"
-      labelClassName="text-xl"
-      childrenContainerClassName="text-xl"
-    >
-      {formatTestScore(testResult.score)}
-    </KeyValue>
+    <>
+      <KeyValue
+        label="Правильно выполнено"
+        childrenContainerClassName="whitespace-nowrap"
+      >
+        {correctAnswers} из {totalAnswers} ({roundTestScore(testResult.score)}%)
+      </KeyValue>
+    </>
   );
 }
