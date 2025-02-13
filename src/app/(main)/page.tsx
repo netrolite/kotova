a038ef3fb8f5d0af12f66fd129eb52439acee0c7
netrolite <67281212+netrolite@users.noreply.github.com>
2025-02-13 12:49:32 +0300

    add link

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,8 @@
 import PageTitle from "@/components/PageTitle";
 import HomepageRecentlyCreatedTests from "@/components/homepage/RecentlyCreatedTests/Index";
 import HomepageRecentlyCreatedTestsSkeleton from "@/components/homepage/RecentlyCreatedTests/Skeleton";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import { Suspense } from "react";
 
 export const metadata = {
@@ -12,7 +14,12 @@ export const metadata = {
 export default function Home() {
   return (
     <>
-      <PageTitle className="mb-8">Недавно созданные тесты</PageTitle>
+      <PageTitle className="mb-1">Недавно созданные тесты</PageTitle>
+      <div className="mb-8">
+        <Link className="underline text-primary" target="_blank" href="https://t.me/+US6-_mwdXTRhNzRi" >
+          Наш Telegram чат
+        </Link>
+      </div>
       <Suspense fallback={<HomepageRecentlyCreatedTestsSkeleton />}>
         <HomepageRecentlyCreatedTests />
       </Suspense>
