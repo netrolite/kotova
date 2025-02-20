28005883176ccbd4475e8b93f3e10be6fa045a0e
netrolite <67281212+netrolite@users.noreply.github.com>
2025-02-20 22:30:03 +0300

    add 2 links to different groups/channels

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,6 @@
 import PageTitle from "@/components/PageTitle";
 import HomepageRecentlyCreatedTests from "@/components/homepage/RecentlyCreatedTests/Index";
 import HomepageRecentlyCreatedTestsSkeleton from "@/components/homepage/RecentlyCreatedTests/Skeleton";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Suspense } from "react";
 
@@ -15,9 +14,20 @@ export default function Home() {
   return (
     <>
       <PageTitle className="mb-1">Недавно созданные тесты</PageTitle>
-      <div className="mb-8">
-        <Link className="underline text-primary" target="_blank" href="https://t.me/+US6-_mwdXTRhNzRi" >
-          Наш Telegram чат
+      <div className="mb-8 flex flex-col gap-1">
+        <Link
+          className="text-primary underline"
+          target="_blank"
+          href="https://t.me/+US6-_mwdXTRhNzRi"
+        >
+          Наш Telegram чат по русскому языку
+        </Link>
+        <Link
+          className="text-primary underline"
+          target="_blank"
+          href="https://t.me/+TPa1gc2OAFRjMDVi"
+        >
+          Наш Telegram чат по литературе
         </Link>
       </div>
       <Suspense fallback={<HomepageRecentlyCreatedTestsSkeleton />}>
